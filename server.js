c5229757a220f081a0382ed6c3be8f563a9fcd55
Lucie Rabahi <lucierabahi@protonmail.com>
2019-07-11 10:32:25 +0200

    Add port

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.post('/imageurl', (req, res) => { imageurl.handleApiCall(req, res) })
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('app is running on port `${process.env.PORT}`');
 });
